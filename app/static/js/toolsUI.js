b667d8d2397597e4d99e2eb876692d5ef9e1869a
Zhengyang Zhao <zzyang@umich.edu>
2025-08-06 11:50:36 -0400

    updated UI

--- app/static/js/toolsUI.js
+++ app/static/js/toolsUI.js
@@ -1,9 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const titleBlock = document.querySelector('.sub-tool-container-item.title');
-  const formBlock = document.querySelector('.sub-tool-container-item.file-form');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const titleBlock = document.querySelector('.sub-tool-container-item.title');
+//   const formBlock = document.querySelector('.sub-tool-container-item.file-form');
 
-  titleBlock.addEventListener('click', function () {
-    const isVisible = formBlock.style.display === 'block';
-    formBlock.style.display = isVisible ? 'none' : 'block';
-  });
-});
+//   titleBlock.addEventListener('click', function () {
+//     const isVisible = formBlock.style.display === 'block';
+//     formBlock.style.display = isVisible ? 'none' : 'block';
+//   });
+// });
+
+window.onload = function() {
+    var popup = document.getElementById('error-popup');
+    if (popup) popup.style.display = 'flex';
+};
